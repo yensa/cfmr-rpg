e401446aa3f55f1cd9f29e10428bd72d14e38197
amateratsu <amaterasu.sempai@gmail.com>
2013-08-28 23:49:44 +0200

    ajout de ligne
    ajout de ligne + d'une boucle pour quitté il faut appuyé sur o

--- tests/menu.py
+++ tests/menu.py
@@ -11,10 +11,27 @@ fenetre = pygame.display.set_mode((640, 480))
 fond = pygame.image.load("menu_teste.jpg").convert()
 fenetre.blit(fond, (0,0))
 
-a=pygame.font.SysFont("Arial",20,True,False)
+a=pygame.font.SysFont("Arial",16,True,False)
 lancement = a.render("lancer",1,(255,255,255))
 fenetre.blit(lancement,(0,0))
 
+b=pygame.font.SysFont("Arial",16,True,False)
+lancement = b.render("option",1,(255,255,255))
+fenetre.blit(lancement,(0,450))
+
+c=pygame.font.SysFont("Arial",16,True,False)
+lancement = c.render("charger",1,(255,255,255))
+fenetre.blit(lancement,(550,450))
+
+d=pygame.font.SysFont("Arial",16,True,False)
+lancement = d.render("charger",1,(255,255,255))
+fenetre.blit(lancement,(550,30))
+
 
 pygame.display.flip()
+
+while True :
+    option=input(">")
+    if option == ("o") :
+        exit()
 
