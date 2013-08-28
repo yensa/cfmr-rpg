776ccfb6906430a74fd61b17370bab99279966e7
amateratsu <amaterasu.sempai@gmail.com>
2013-08-28 23:17:51 +0200

    teste sur le menu
    initial commit

---
+++ tests/menu.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+
+import pygame.mixer
+import pygame
+
+from pygame.locals import *
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((640, 480))
+fond = pygame.image.load("menu_teste.jpg").convert()
+fenetre.blit(fond, (0,0))
+
+a=pygame.font.SysFont("Arial",16,True,False)
+lancement = a.render("lancer",1,(255,255,255))
+fenetre.blit(lancement,(0,0))
+
+
+pygame.display.flip()
+
