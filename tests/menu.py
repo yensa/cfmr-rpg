3caad51eab566daf41f4cbac1c64b016871d707b
amateratsu <amaterasu.sempai@gmail.com>
2013-08-29 21:33:10 +0200

    modif code pour quité
    code pour quité qui ne marche pas

--- tests/menu.py
+++ tests/menu.py
@@ -8,7 +8,7 @@ from pygame.locals import *
 pygame.init()
 
 pygame.mixer.music.load("menu.ogg")
-pygame.mixer.music.play()
+pygame.mixer.music.play(-1)
 
 fenetre = pygame.display.set_mode((640, 480))
 fond = pygame.image.load("menu_teste.jpg").convert()
@@ -30,11 +30,18 @@ d=pygame.font.SysFont("Arial",16,True,False)
 lancement = d.render("charger",1,(255,255,255))
 fenetre.blit(lancement,(550,30))
 
-
 pygame.display.flip()
 
-while True :
-    option=input(">")
-    if option == ("q") :
-        exit()
-
+while continuer:
+    for event in pygame.event.get():   
+        if event.type == QUIT:     
+            continuer = 0
+        elif event.type ==K_DOWN :
+            pass
+        elif event.type ==K_RIGHT :
+            pass
+        elif event.type ==K_UP :
+            pass
+        elif event.type ==K_LEFT :
+            pass
+        
