6d9778938395eaefb4bc4ee69e6d89b40a3d5d3c
amateratsu <amaterasu.sempai@gmail.com>
2013-09-08 00:26:36 +0200

    les 3 images
    modification du code pour qu'il marche

--- src/trois_images.py
+++ src/trois_images.py
@@ -11,16 +11,14 @@ class Test(Scene):
 		self._loader.preload_image(("icon", "icon.png"))
 		self._drawables.append(Image(self._loader.images.icon, (320, 240), CENTER))
 		self._loader.preload_image(("drapeau", "drapeau.png"))
-		self._drawables.append(Image(self._loader.images.icon, (290, 210), CENTER))
+		self._drawables.append(Image(self._loader.images.drapeau, (290, 210), CENTER))
 		self._loader.preload_image(("pomme", "pomme.png"))
-		self._drawables.append(Image(self._loader.images.icon, (250, 170), CENTER))
+		self._drawables.append(Image(self._loader.images.pomme, (250, 170), CENTER))
 
 
 app = Application("../resources")
 
-window = Window(config={"caption": "Hello World","icon": "C:/Users/chris/Documents/GitHub/cfmr-rpg/resources/textures/icon.png",
-                        "C:/Users/chris/Documents/GitHub/cfmr-rpg/resources/textures/drapeau.png",
-                        "C:/Users/chris/Documents/GitHub/cfmr-rpg/resources/textures/pomme.png"})
+window = Window(config={"caption": "Hello World","icon": "C:/Users/chris/Documents/GitHub/cfmr-rpg/resources/textures/icon.png"})
 
 app.register_window(window)
 
